d56e39e5bf524b85322daac79f93dae685eaae3f
RodrigoPLpz <rodrigopi51.49@gmail.com>
2019-09-26 11:53:53 -0500

    se agrego condicion

--- main.js
+++ main.js
@@ -1,17 +1,26 @@
 var x = document.querySelector('#personaje');
 function mostrar(){
-    fetch('https://rickandmortyapi.com/api/character/')
-    .then(res => res.json())
-    .then(data => {
-        // console.log(data.results[0].episode[0]);
+    var y = document.getElementById("opc").value;
+    if(y>=0 && y<=19){
+        fetch('https://rickandmortyapi.com/api/character/')
+        .then(res => res.json())
+        .then(data => {
+            // console.log(data.results[0].episode[0]);
+            // console.log(y);
+            personaje.innerHTML= `
+            <img src="${data.results[y].image}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h4 class="card-title">${data.results[y].name}</h4>
+                <h5 class="card-title">${data.results[y].gender}</h5>
+                <p class="card-text">${data.results[y].status}</p>
+                <a href="#" class="btn btn-primary" onclick="location.reload()">clic</a>
+            </div>
+            `
+        })
+    } else{
         personaje.innerHTML= `
-        <img src="${data.results[0].image}" class="card-img-top" alt="...">
-        <div class="card-body">
-            <h4 class="card-title">${data.results[0].name}</h4>
-            <h5 class="card-title">${data.results[0].gender}</h5>
-            <p class="card-text">${data.results[0].status}</p>
-            <a href="#" class="btn btn-primary" onclick="mostrar()">clic</a>
-        </div>
+        <p class="card-text">Error</p>
+        <a href="#" class="btn btn-primary" onclick="location.reload()">clic</a>
         `
-    })
+    }
 }
