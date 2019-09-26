33d6f6b8ba8b3d96399b5d0c1c8113bfc52d5fc3
RodrigoPLpz <rodrigopi51.49@gmail.com>
2019-09-25 21:27:28 -0500

    cambios funcional

--- main.js
+++ main.js
@@ -1,23 +1,17 @@
-window.addEventListener("DOM",() =>{
-    let out ='';
-    let out2 = '';
-
-    fetch(`https://rickandmortyapi.com/api/character/$`)
-        .then(res => res.json())
-        .then(data => {
-            let personaje = data.results;
-            muestraPer(personaje);
-        })
-        .catch(err => {
-            out2.innerHTML = "Error"
-
-        })
-
-    function muestraPer(personaje){
-        personaje.forEach(per => {
-            
-        });
-
-    }
-
-});
+var x = document.querySelector('#personaje');
+function mostrar(){
+    fetch('https://rickandmortyapi.com/api/character/')
+    .then(res => res.json())
+    .then(data => {
+        // console.log(data.results[0].episode[0]);
+        personaje.innerHTML= `
+        <img src="${data.results[0].image}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h4 class="card-title">${data.results[0].name}</h4>
+            <h5 class="card-title">${data.results[0].gender}</h5>
+            <p class="card-text">${data.results[0].status}</p>
+            <a href="#" class="btn btn-primary" onclick="mostrar()">clic</a>
+        </div>
+        `
+    })
+}
